52e78978dfa59d34b12f37582516005cab10315b
camt71 <22130308@st.hcmuaf.edu.vn>
2025-06-23 02:41:01 +0700

    commit trước khi merge main

--- Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/model/FriendListResponse.java
+++ Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/model/FriendListResponse.java
@@ -4,7 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-//total friend, max friend, friends, suggestions???
 public class FriendListResponse {
     @SerializedName("friends")
     private List<User> friends;
